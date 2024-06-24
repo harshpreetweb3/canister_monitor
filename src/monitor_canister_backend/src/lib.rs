9aa352e58c3fec9325add9122ccdef94a6b898a2
harshpreetweb3 <harshpreetweb3@gmail.com>
2024-06-24 12:16:30 +0530

    in process

--- src/monitor_canister_backend/src/lib.rs
+++ src/monitor_canister_backend/src/lib.rs
@@ -1,16 +1,25 @@
 mod canister_status;
+mod get_canister_status;
 mod memory;
 mod state;
 mod store_canister_status;
 mod types;
 
-mod get_canister_status;
+mod archive;
+mod blockchain;
+mod cdk_runtime;
+mod range_utils;
+
+pub mod runtime;
+pub mod spawn;
 
 use get_canister_status::get_canister_status;
 use memory::*;
 use state::*;
 use store_canister_status::set_canister_map;
 use types::*;
+
+use std::borrow::Cow;
 
 use candid::Principal;
 use ic_cdk::{
@@ -26,20 +35,46 @@ use ic_cdk::{
 
 use ic_cdk_timers;
 use std::time::Duration;
-
 const N: Duration = Duration::from_secs(5);
 
-// async fn ring() -> String {
-//     ic_cdk::println!("Rust Timer Ring!");
-//     "Ring".to_string()
-// }
+
+use std::sync::{Arc, RwLock};
+// use ledger_canister_core::blockchain::Blockchain;
+
+use archive::ArchiveOptions;
+use archive::ArchiveCanisterWasm;
+use blockchain::Blockchain;
+use cdk_runtime::CdkRuntime;
+
+#[derive(Debug, Clone)]
+pub struct MyArchiveWasm;
+
+impl ArchiveCanisterWasm for MyArchiveWasm {
+    fn archive_wasm() -> Cow<'static, [u8]> {
+        Cow::Borrowed(include_bytes!("/home/harshpreet-singh/Documents/new-project/monitor_canister/target/wasm32-unknown-unknown/release/archive.wasm"))
+    }
+}
+
 
 #[ic_cdk::init]
-async fn init() {
+async fn init(archive_options: ArchiveOptions) {
+    
+    let blockchain = Blockchain::<CdkRuntime, MyArchiveWasm>::new_with_archive(archive_options);
+
     ic_cdk_timers::set_timer_interval(N, || {
         ic_cdk::spawn(get_canister_status());
     });
 }
+
+export_candid!();
+
+// get remaining cycles from canister_status
+//1. all matrices, fun for all mat..
+//2. store in a stable memory
+//3. each minute, call and store matric ; heartbeats
+//4, show matric // 5 minute, 10 min , from  frontend | graph
+//graph for each
+//get storage, get cycles
 
 //without CallResult return type
 // #[ic_cdk::query]
@@ -81,13 +116,3 @@ async fn init() {
 // pub async fn get_canister_status(arg: CanisterIdRecord) -> CallResult<(CanisterStatusResponse,)> {
 //     ic_cdk::call(Principal::management_canister(), "canister_status", (arg,)).await
 // }
-
-export_candid!();
-
-// get remaining cycles from canister_status
-//1. all matrices, fun for all mat..
-//2. store in a stable memory
-//3. each minute, call and store matric ; heartbeats
-//4, show matric // 5 minute, 10 min , from  frontend | graph
-//graph for each
-//get storage, get cycles
